6198d9629a1c33d174877a1a47a95b140a8fafcb
azirconia <dev1.aga@example.com>
2026-03-08 15:07:03 +0100

    moj pierwszy commit git

---
+++ gry.py
@@ -0,0 +1,24 @@
+import random
+opcie = ["papier","kamień","nozyce"]
+
+while True:
+    gracz = input("wybierz kamień papier lub nożyce (albo napisz koniec)")
+
+    if gracz == "koniec":
+        print("dobra gra")
+        break
+    komputer = random.choice(opcie)
+    print("komputer wybrał:", komputer)
+    
+    if gracz == komputer:
+        print("remis")
+    elif (gracz == "kamien" and komputer == "nozyce") or \
+            (gracz == "papier" and komputer == "kamien") or \
+            (gracz == "nozyce" and komputer == "papier"):
+        print("wygrałeś!🎉")
+    else:
+        print("komputer wygrał!🤖")
+
+    
+    
+     
